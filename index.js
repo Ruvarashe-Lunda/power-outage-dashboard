0a9727d07090925c73a459e3e23ca982890109c0
primlunda <primrose.lunda@publicis.mu>
2025-08-19 12:03:02 +0400

    to make changes

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-function getData() {
+async function getData() {
   const URL =
     "https://raw.githubusercontent.com/MrSunshyne/mauritius-dataset-electricity/refs/heads/main/data/power-outages.latest.json";
-  fetch(URL)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      return data;
-    });
+  let result = await fetch(URL);
+  let json = await result.json();
+  return json;
 }
 getData();
 
@@ -14,19 +11,52 @@ function extractFuture(obj) {
   return obj["future"];
 }
 
-function initialize() {
-  const result = getData();
-  const future = extractFuture(result);
-  addToDom("message");
-  console.log(future);
-}
-function addToDom(item) {
+function clearOutput() {
   const output = document.getElementById("output");
-  let wrapper = document.getElementById("div");
-  wrapper.innerHTML = item;
-  output.innerHTML += wrapper;
+  output.innerHTML = "";
 }
 
+function renderOutput(items){
+      for (let index = 0; index < future.length; index++) {
+    const outage = future[index];
+    addToDom(outage);
+  }
+  console.log(future);
+}
+
+async function initialize() {
+  const result = await getData();
+  const future = extractFuture(result);
+
+  clearOutput();
+  renderOutput(future)
+
+
+}
+
+function addToDom(item) {
+  const output = document.getElementById("output");
+  // let wrapper = document.createElement('div')
+
+  for (let i = 0; i < item.length; i++) {
+    const templateCard = `
+            <div class="card border rounded-full space-y-4 p-10 bg-white">
+            <div class="text-center font-medium">${item[i].date}</div>
+            <div class="flex justify-between" >
+            <div>${outage.locality}</div>
+                <div>${outage.dictrict}</div>
+            </div>
+            <div class="flex justify-between">
+                <div>${outage.from}</div>
+                <div>${outage.to}</div>
+            </div>
+            </div>
+       
+    `;
+    console.log(output);
+    output.innerHTML += templateCard;
+  }
+}
 document.addEventListener("DOMContentLoaded", () => {
   initialize();
 });
